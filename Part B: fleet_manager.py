24f0f709c94675e5b3dd13195625b8c3e684dd03
AlexPadoodoo <app10@kent.ac.uk>
2026-02-08 15:22:58 +0000

    display_menu function

--- Part B: fleet_manager.py
+++ Part B: fleet_manager.py
@@ -4,3 +4,23 @@ def init_database():
     divs = ["Command", "Operations", "Engineering", "Security", "Sciences"]
     ids = ["NCC-1701-01", "NCC-1701-02", "NCC-1701-03", "NCC-1701-04", "NCC-1701-05"]
     return names, ranks, divs, ids
+
+
+def display_menu(user_name):
+    print("\n" + "="*30)
+    print("FLEET MANAGEMENT SYSTEM")
+    print("--------------------------")
+    print(f"Logged in as: " + user_name)
+    print("="*30)
+    print("1. View Roaster")
+    print("2. Add Crew Member")
+    print("3. Remove Crew Member")
+    print("4. Update Rank")
+    print("5. Search Crew")
+    print("6. Filter by Divison")
+    print("7. Calculate Payroll")
+    print("8. Count Senior Officers")
+    print("9. Exit")
+
+    choice = input("\n Select an option (1-9): ")
+    return choice
