7d1d7fceb0a8e149480b8ce61cc7fad30017cd16
AlexPadoodoo <app10@kent.ac.uk>
2026-02-08 14:33:48 +0000

    Made the fuel decrease

--- Part A: old_system.py
+++ Part A: old_system.py
@@ -25,10 +25,10 @@ def run_system_monolith():
         
         opt = input("Select option: ")
         
-        if opt == "1":  #Syntax Error, should be double equals sign
+        if opt == "1":  #Syntax Error, Bug 2 fix: should be double equals sign
             print("Current Crew List:")
             
-            for i in range(len(n)): #Index error: loops until it reaches 10, so the code crashes once it reaches 4
+            for i in range(len(n)): #Index error: loops until it reaches 10, so the code crashes once it reaches 4. Bug 3 fix.
                 print(n[i] + " - " + r[i]) 
                 
         elif opt == "2":
@@ -38,20 +38,20 @@ def run_system_monolith():
             
            
             n.append(new_name)
-            r.append(new_rank) #Keeps the data in sync
+            r.append(new_rank) #Bug 4 fix: Keeps the data in sync
             r.append(new_div)
             print("Crew member added.")
             
         elif opt == "3":
             rem = input("Name to remove: ")
            
-            if rem in n: #Checks in the name exists to avoid any value errors
+            if rem in n: #Bug 5 fix: Checks in the name exists to avoid any value errors
                 idx = n.index(rem)
                 n.pop(idx)
                 r.pop(idx)
                 d.pop(idx)
                 print("Removed.")
-            else:   #Added an else function to stop the code crashing
+            else:   #Bug 6 fix: Added an else function to stop the code crashing
                 print("Name not found")
             
         elif opt == "4":
@@ -59,9 +59,9 @@ def run_system_monolith():
             count = 0
             
             for rank in r:
-                if rank == "Captain" or rank == "Commander": #Corrected logical comparison
+                if rank == "Captain" or rank == "Commander": #Bug 7 fix: Corrected logical comparison
                     count = count + 1
-            print("High ranking officers: " + str(count)) #Cast count to string to avoid TypeError
+            print("High ranking officers: " + str(count)) #Bug 8 fix: Cast count to string to avoid TypeError
             
         elif opt == "5":
             print("Shutting down.")
@@ -89,8 +89,9 @@ def run_system_monolith():
         while fuel > 0:
             
             print("Idling...")
+            fuel -= 50 #Bug 9 Fix: Fuel actually decreases now
             break 
             
         print("End of cycle.")
 
-run_system_monolith() #Added parentheses to call the function
+run_system_monolith() #Bug 10 fix: Added parentheses to call the function
