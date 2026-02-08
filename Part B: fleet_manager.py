a2847af207b8ce9816b6a99b6ac0ee9500b40778
AlexPadoodoo <app10@kent.ac.uk>
2026-02-08 15:01:46 +0000

    done the first function

---
+++ Part B: fleet_manager.py
@@ -0,0 +1,6 @@
+def init_database():
+    names = ["Jean Picard", "William Riker", "Worf", "Deanna Troi", "Geordi La Froge"] #5 Star trek character
+    ranks = ["Capitain", "Commander", "Lt. Commander", "Lieutenant", "Commander"]
+    divs = ["Command", "Operations", "Engineering", "Security", "Sciences"]
+    ids = ["NCC-1701-01", "NCC-1701-02", "NCC-1701-03", "NCC-1701-04", "NCC-1701-05"]
+    return names, ranks, divs, ids
