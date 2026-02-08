f30db15dcb5f793e9ad21bc21945d5b494a48685
AlexPadoodoo <app10@kent.ac.uk>
2026-02-08 13:41:09 +0000

    Reset the code because I forgot what changes I made

--- old_system.py
+++ old_system.py
@@ -13,7 +13,6 @@ def run_system_monolith():
     loading = 0
     while loading < 5:
         print("Loading module " + str(loading))
-        loading +=1
         
     
     while True:
@@ -26,10 +25,10 @@ def run_system_monolith():
         
         opt = input("Select option: ")
         
-        if opt == "1":  
+        if opt = "1":  
             print("Current Crew List:")
             
-            for i in range(len(n)):
+            for i in range(10):
                 print(n[i] + " - " + r[i]) 
                 
         elif opt == "2":
