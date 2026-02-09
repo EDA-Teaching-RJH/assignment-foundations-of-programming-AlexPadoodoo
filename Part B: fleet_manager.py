c7361d8c96c56ca39ae6ae0dd01a66cf8beafaa1
AlexPadoodoo <app10@kent.ac.uk>
2026-02-09 18:50:06 +0000

    made the IDs much simpler

--- Part B: fleet_manager.py
+++ Part B: fleet_manager.py
@@ -3,7 +3,7 @@ def init_database():
     names = ["Jean Picard", "William Riker", "Worf", "Deanna Troi", "Geordi La Froge"] #5 Star trek character
     ranks = ["Capitain", "Commander", "Lt. Commander", "Lieutenant", "Commander"]
     divs = ["Command", "Operations", "Engineering", "Security", "Sciences"]
-    ids = ["NCC-1701-01", "NCC-1701-02", "NCC-1701-03", "NCC-1701-04", "NCC-1701-05"]
+    ids = ["101", "102", "103", "104", "105"]
     return names, ranks, divs, ids
 
 #Function 2:
