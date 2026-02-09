b7d4af7c7263ef4b821adb1aedc9003d12fd8750
AlexPadoodoo <app10@kent.ac.uk>
2026-02-09 18:35:55 +0000

    created the Main function

--- Part B: fleet_manager.py
+++ Part B: fleet_manager.py
@@ -127,3 +127,40 @@ def count_officers(ranks):
         if rank == "Captain" or rank == "Commander":
             count +=1
     return count
+
+
+
+#Main function
+
+def main():
+    names, ranks, divs, ids = init_database()
+    user_name = "Alex Paduraru"
+
+    while True:
+        choice = display_menu(user_name)
+
+        if choice == "1":
+            display_roster(names, ranks, divs, ids)
+        elif choice == "2":
+            add_member(names, ranks, divs, ids)
+        elif choice == "3":
+            remove_member(names,ranks,divs,ids)
+        elif choice == "4":
+            update_rank(names, ranks, ids)
+        elif choice == "5":
+            search_crew(names, ranks, divs, ids)
+        elif choice == "6":
+            filter_by_divison(names, divs)
+        elif choice == "7":
+            total_cost = calculate_payroll(ranks)
+            print(f"Total Crew Payroll: {total_cost}")
+        elif choice == "8":
+            officer_count = count_officers(ranks)
+            print(f"Total Senior Officers: {officer_count}")
+        elif choice == "9":
+            print("Shutting down system...")
+            break
+        else:
+            print("Invalid Selection. Try again.")
+
+main()
